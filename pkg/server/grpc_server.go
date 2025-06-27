9de54f6fe68191220e62cac07f05bf27af2f4e35
Nicolas PLANEL <nplanel@cisco.com>
2025-06-27 17:42:01 +0200

    new API Add/Delete Path(s)

--- pkg/server/grpc_server.go
+++ pkg/server/grpc_server.go
@@ -417,9 +417,7 @@ func api2Path(resource api.TableType, path *api.Path, isWithdraw bool) (*table.P
 		}
 	}
 
-	if nlri == nil {
-		return nil, fmt.Errorf("nlri not found")
-	} else if !path.IsWithdraw && nexthop == "" {
+	if !path.IsWithdraw && nexthop == "" {
 		return nil, fmt.Errorf("nexthop not found")
 	}
 	rf := bgp.AfiSafiToFamily(uint16(path.Family.Afi), uint8(path.Family.Safi))
